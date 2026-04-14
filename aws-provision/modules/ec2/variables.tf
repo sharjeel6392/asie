7eb1e077d6222cf0fb33e5b0fedcf066fc8b3adf
Sharjeel <sharjeel6392@gmail.com>
2026-04-15 02:52:34 +0530

    fixing terraform scripts

--- aws-provision/modules/ec2/variables.tf
+++ aws-provision/modules/ec2/variables.tf
@@ -2,3 +2,7 @@ variable "vpc_id" {}
 variable "public_subnet_id" {}
 variable "private_subnet_id" {}
 variable "ami" {}
+variable "my_ip" {
+    description = "The IP address fetched from the user's machine to allow access to the bastion host."
+    type        = list(string) 
+}
