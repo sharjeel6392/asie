91e25f5e23667ff39d43f85c1778ba1ceddfa768
Sharjeel <sharjeel6392@gmail.com>
2026-04-27 12:56:45 +0530

    implemented a shadow-aware production export system

--- src/models/export_model.py
+++ src/models/export_model.py
@@ -4,41 +4,56 @@ import mlflow
 from mlflow import tracking
 from mlflow import artifacts
 
-from models.model_resolver import get_promoted_model
+from src.models.model_registry import load_registry
 from serving.config import Settings
 
 EXPORT_DIR = "exported_model"
 
-def export():
-    models = get_promoted_model()
+def _download(run_id: str, artifact_path: str) -> str:
+    uri = f'runs:/{run_id}/{artifact_path}'
+    return artifacts.download_artifacts(uri)
+
+def _copy(src, dst):
+    shutil.copytree(src, dst)
+
+def export_models():
+    registry = load_registry()
 
     mlflow.set_tracking_uri(Settings.MLFLOW_TRACKING_URI)
-    print(tracking.get_tracking_uri)
 
     if os.path.exists(EXPORT_DIR):
-            shutil.rmtree(EXPORT_DIR)
+        shutil.rmtree(EXPORT_DIR)
 
-    for model in models:
-        print(f'Loading {model['type']} model')
-        
-        model_uri = f'runs:/{model['run_id']}/model'
-        tokenizer_uri = f'runs:/{model['run_id']}/tokenizer'
+    os.makedirs(EXPORT_DIR, exist_ok=True)
 
-        print(f'Model_URI: {model_uri}\nTokenizer URI: {tokenizer_uri}')
+    # Export Primary
+    primary = registry.get('primary')
 
-        print('Downloading model...')
-        model_path = artifacts.download_artifacts(model_uri)
+    if not primary:
+        raise ValueError('No primary model found in registry')
+    
+    print('Exporting Primary mode...')
 
-        print('Downloading tokenizer...')
-        tokenizer_path = artifacts.download_artifacts(tokenizer_uri)
+    model_path = _download(primary['run_id'], 'model')
+    tokenizer_path = _download(primary['run_id'], "tokenizer")
 
-        os.makedirs(EXPORT_DIR, exist_ok=True)
+    _copy(model_path, os.path.join(EXPORT_DIR, 'primary', 'model'))
+    _copy(tokenizer_path, os.path.join(EXPORT_DIR, 'primary', 'tokenizer'))
 
-        shutil.copytree(model_path, os.path.join(EXPORT_DIR, model['type'], "model"))
-        shutil.copytree(tokenizer_path, os.path.join(EXPORT_DIR, model['type'], "tokenizer"))
+    # Export Shadow (if exists)
+    shadow = registry.get('shadow')
 
+    if shadow:
+        print('Exporting shadow model...')
+
+        model_path = _download(shadow['run_id'], 'model')
+        tokenizer_path = _download(shadow['run_id'], 'tokenizer')
+
+        _copy(model_path, os.path.join(EXPORT_DIR, "shadow", "model"))
+        _copy(tokenizer_path, os.path.join(EXPORT_DIR, 'shadow', 'tokenizer'))
+    
     print('Export complete')
 
 
 if __name__ == '__main__':
-    export()
+    export_models()
